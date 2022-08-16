0c80ab72c4ecae3a5c2c57413f950aff2c18606f
Mou <sunmou@google.com>
2022-08-16 03:42:38 -0700

    read test result

--- read_test_result.py
+++ read_test_result.py
@@ -5,7 +5,6 @@ def main():
   parser = argparse.ArgumentParser(description='Build for iOS and tvOS.')
   parser.add_argument('-r', '--test_result', default=None)
   args = parser.parse_args()
-  print("--test_result: %s" % args.test_result)
   test_result = json.loads(args.test_result)
   print("project_id: %s" % test_result.get("project_id"))
   for app in test_result.get("apps"):
