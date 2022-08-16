20edd19abac2caafdc95f0662052dd7a8ffaed1e
Mou <sunmou@google.com>
2022-08-15 19:28:41 -0700

    read test result

---
+++ read_test_result.py
@@ -0,0 +1,15 @@
+import argparse
+import json
+
+def main():
+  parser = argparse.ArgumentParser(description='Build for iOS and tvOS.')
+  parser.add_argument('-r', '--test_result', default=None)
+  args = parser.parse_args()
+  test_result = json.loads(args.test_result)
+  print("test_result: %s" % test_result)
+  print("project_id: %s" % test_result.get("project_id"))
+  for app in test_result.get("apps"):
+    print("return_code: %s; ftl_link: %s" % (app.get("return_code"), app.get("ftl_link")))
+
+if __name__ == '__main__':
+  main()
