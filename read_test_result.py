8d9d31f9700eb6d6aea539d7df2ecfaa40e94e21
Mou <sunmou@google.com>
2022-08-16 02:27:20 -0700

    read test result

--- read_test_result.py
+++ read_test_result.py
@@ -1,16 +1,7 @@
 import argparse
 import json
-import logging
-from absl import logging
 
 def main():
-  test = { "abc": 12 }
-  print("test: %s" % test)
-  logging.error("test: %s" % test)
-  print("test: %s" % json.dumps(test))
-  test = { "abc": 12, "def": [{"a":"a", "b":"b"}, {"a":"a", "b":"b"}] }
-  print("test: %s" % test)
-  print("test: %s" % json.dumps(test))
   parser = argparse.ArgumentParser(description='Build for iOS and tvOS.')
   parser.add_argument('-r', '--test_result', default=None)
   args = parser.parse_args()
