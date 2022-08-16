22f4fbbd04233b1d6fa88948d9f28f2592cb2c45
Mou <sunmou@google.com>
2022-08-15 20:08:18 -0700

    read test result

--- read_test_result.py
+++ read_test_result.py
@@ -2,6 +2,9 @@ import argparse
 import json
 
 def main():
+  test = { "abc": 12 }
+  print("test: %s" % test)
+  print("test: %s" % json.dumps(test))
   test = { "abc": 12, "def": [{"a":"a", "b":"b"}, {"a":"a", "b":"b"}] }
   print("test: %s" % test)
   print("test: %s" % json.dumps(test))
